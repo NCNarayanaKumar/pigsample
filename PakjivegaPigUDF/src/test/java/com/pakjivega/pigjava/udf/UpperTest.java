7d0a64a93da8ff7d500f7dde0267a5e1b1cd417f
Jorge de la Vega <pakjivega@gmail.com>
2014-11-01 02:59:30 +0100

    Update to junit 4.11

--- PakjivegaPigUDF/src/test/java/com/pakjivega/pigjava/udf/UpperTest.java
+++ PakjivegaPigUDF/src/test/java/com/pakjivega/pigjava/udf/UpperTest.java
@@ -5,51 +5,52 @@ import java.io.IOException;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
 
-import junit.framework.Test;
+import org.junit.Assert;
+import org.junit.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
  * Unit test for simple Upper.
  */
-public class UpperTest extends TestCase
+public class UpperTest
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public UpperTest( String testName )
+    public UpperTest(  )
     {
-        super( testName );
+        
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( UpperTest.class );
-    }
+//    public static Test suite()
+//    {
+//        return new TestSuite( UpperTest.class );
+//    }
 
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testUpper()
     {
     	try {
     		Tuple tuple = TupleFactory.getInstance().newTuple();
         	tuple.append("John");
-        	assertEquals("JOHN", new Upper().exec(tuple) );
+        	Assert.assertEquals("JOHN", new Upper().exec(tuple) );
         	
         	Tuple tuple2 = TupleFactory.getInstance().newTuple();
         	tuple2.append("");
-        	assertEquals("", new Upper().exec(tuple2) );
+        	Assert.assertEquals("", new Upper().exec(tuple2) );
         	
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        assertTrue( true );
     }
 }
